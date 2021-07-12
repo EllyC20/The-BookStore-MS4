9a51206ebe27c88a0cdc8aed202722a3c0a9ceac
EllyC20 <callanelly@yahoo.com>
2021-07-12 12:37:17 +0000

    Add products app

---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    name = models.CharField(max_length=300)
+    author = models.CharField(max_length=300)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey("Category", null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
