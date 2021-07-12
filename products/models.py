9dd278fc2e879849a072927f440551b5af17d092
EllyC20 <callanelly@yahoo.com>
2021-07-12 16:20:11 +0000

    Add correct settings to display product images

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     name = models.CharField(max_length=300)
     author = models.CharField(max_length=300)
     description = models.TextField(null=True, blank=True)
