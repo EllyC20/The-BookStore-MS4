7614b66354c96e2e7989eeaee9b0aaae90db4c40
EllyC20 <callanelly@yahoo.com>
2021-08-10 13:53:00 +0000

    Add keys to the context

--- checkout/views.py
+++ checkout/views.py
@@ -9,11 +9,13 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag.")
         return redirect(reverse('products'))
-    
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IxsblLGzLkJihrMvjNETq6KECljrf4rkbW9GckbRykoIpsbkhYvxLI2oSGxj4ceKV7ixAKsvHk3jNiwuVK8pp5200Yh8iXFHc',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
