abd49db9886c226070a8f579a78e1ba8f5b77af2
EllyC20 <callanelly@yahoo.com>
2021-08-25 23:31:32 +0000

    Update db.json file

--- the_bookstore/settings.py
+++ the_bookstore/settings.py
@@ -134,6 +134,13 @@ else:
     }
 
 
+""" DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+} """
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
