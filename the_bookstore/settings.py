ad27ed5d4bc520fc65beba4529e40dffa2fb6ced
EllyC20 <callanelly@yahoo.com>
2021-08-26 09:19:49 +0000

    Adjust debug setting

--- the_bookstore/settings.py
+++ the_bookstore/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY_MS4")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['the-bookstore-ms4.herokuapp.com', 'localhost']
 
