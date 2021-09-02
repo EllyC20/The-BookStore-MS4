da62ee0e55ebaa0f7c7ec5b8b96d4b01529b4092
EllyC20 <callanelly@yahoo.com>
2021-09-02 12:13:36 +0000

    Add fix to postloadjs block to allow toast messages to show

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.core.paginator import Paginator
-from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.db.models.functions import Lower
 from .models import Product, Category, ProductReview
 from .forms import ReviewForm, ProductForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -40,11 +40,10 @@ def all_products(request):
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "Sorry - no results!")
+                messages.error(request, "Sorry No Results!")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(author__icontains=query)
